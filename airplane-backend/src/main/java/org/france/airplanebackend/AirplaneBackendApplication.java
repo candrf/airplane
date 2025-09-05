2199647e2a7087b87c3b0dd0f04c86b8155f9ab7
Andrew France <andrew.c.france.mil@army.mil>
2025-09-05 09:38:58 -0500

    initial; project build

---
+++ airplane-backend/src/main/java/org/france/airplanebackend/AirplaneBackendApplication.java
@@ -0,0 +1,13 @@
+package org.france.airplanebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirplaneBackendApplication.class, args);
+    }
+
+}
